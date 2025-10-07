86cf0839f304558a7e9a8c05cba4d97aa3290450
tzwf0819 <tzwf0819@example.com>
2025-10-07 10:00:24 +0800

    修复了部署配置错误

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,11 +9,7 @@ module.exports = {
     storage: ':memory:'
   },
   production: {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || '3306',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || 'password',
-    database: process.env.DB_NAME || 'factory_management',
-    dialect: 'mysql'
+    dialect: 'sqlite',
+    storage: './database/shangyin.db'
   }
 };
